4419ae5d09bc2dfc18e1ec54a9fc4a7a38f820a9
Caio Silva <caio.goncalvesananias@gmail.com>
2026-05-19 10:12:12 -0300

    feat: add Cadastro page and update routing; enhance Login page with navigation

--- no-sufoco/src/App.jsx
+++ no-sufoco/src/App.jsx
@@ -1,3 +1,4 @@
+import Cadastro from "./pages/Cadastro";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/cadastro" element={<Cadastro />}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- no-sufoco/src/pages/Login.jsx
+++ no-sufoco/src/pages/Login.jsx
@@ -9,6 +9,7 @@ import InputAdornment from "@mui/material/InputAdornment";
 import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
@@ -19,6 +20,8 @@ function Login() {
   const [senha, setSenha] = useState("");
 
   const [showPassword, setShowPassword] = React.useState(false);
+
+  const navigate = useNavigate();
 
   const handleClickShowPassword = () => {
     setShowPassword((show) => !show);
@@ -41,19 +44,26 @@ function Login() {
     };
 
     try {
-      const response = await axios.post("https://backend-no-sufoco.vercel.app/api/user/login", dadosEnvio);
-        alert("Login realizado!");
-        console.log(response)
+      const response = await axios.post(
+        "https://backend-no-sufoco.vercel.app/api/user/login",
+        dadosEnvio,
+      );
+      alert("Login realizado!");
+      console.log(response);
+      navigate('/dashboard')
     } catch (error) {
       console.error("Erro ao enviar: ", error);
     }
   };
 
-
   return (
     <div className="h-screen w-screen flex items-center justify-center">
+      <div className="flex flex-col justify-center gap-5">
+    <a href="/cadastro">Não é cadastrado ainda? Registre-se!</a>
       <form onSubmit={handleSubmit}>
+          
         <div className="grid gap-3">
+          
           <TextField
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -61,7 +71,7 @@ function Login() {
             variant="outlined"
           />
 
-          <FormControl sx={{ width: "25ch" }} variant="outlined">
+          <FormControl sx={{ width: '100%' }} variant="outlined">
             <InputLabel htmlFor={`${outlinedPasswordId}-input`}>
               Senha
             </InputLabel>
@@ -94,6 +104,7 @@ function Login() {
           </Button>
         </div>
       </form>
+      </div>
     </div>
   );
 }
